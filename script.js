de7b2d99891a807322aaac5db414a90af7b1848f
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2022-10-21 22:56:23 +0330

    question 4

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// question3
-const data = [
-  { name: "soli", age: 31 },
-  { name: "sofi", age: 15 },
-  { name: "rahi", age: 50 },
-  { name: "nasim", age: 40 },
-  { name: "elly", age: 60 },
-  { name: "sahar", age: 10 },
-  { name: "zahra", age: 80 },
-];
-
-const sortByAge = (data) => {
-  return data.sort((a, b) => a.age - b.age);
+// question 4
+let up = prompt("top");
+let right = prompt("right");
+let html = prompt("html");
+let className = prompt("choose a classname(blue,green or red)");
+console.log(html);
+const commentAndPlace = (top = 0, right = 0, html, className) => {
+  const comment = document.createElement("p");
+  comment.innerText = html;
+  comment.style.position = "absolute";
+  comment.style.top = `${top.length >0 ?top : 0}px`;
+  comment.style.right = `${right.length >0 ?right : 0}px`;
+  comment.className = className
+  document.body.append(comment);
 };
-console.log(sortByAge(data));
+commentAndPlace(up, right, html, className);
