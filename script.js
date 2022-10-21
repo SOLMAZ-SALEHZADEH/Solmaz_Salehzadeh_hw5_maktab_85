01dfe6c6c7116ddec53b96c27edaf7eb1314f0ee
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2022-10-21 23:00:47 +0330

    question 5

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// question 4
-let up = prompt("top");
-let right = prompt("right");
-let html = prompt("html");
-let className = prompt("choose a classname(blue,green or red)");
-console.log(html);
-const commentAndPlace = (top = 0, right = 0, html, className) => {
-  const comment = document.createElement("p");
-  comment.innerText = html;
-  comment.style.position = "absolute";
-  comment.style.top = `${top.length >0 ?top : 0}px`;
-  comment.style.right = `${right.length >0 ?right : 0}px`;
-  comment.className = className
-  document.body.append(comment);
-};
-commentAndPlace(up, right, html, className);
+// question 5
+
+const ask = (question,yes,no)=>{
+  if (confirm(question)) yes()
+  else no()
+}
+ask(
+ "Do you agree ?",
+ function() { alert("You agreed."); },
+ function() { alert("You canceled the execution."); }
+);
